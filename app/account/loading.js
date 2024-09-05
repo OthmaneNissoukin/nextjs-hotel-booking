624afc97281bba840f9dffaae1b088ca126b1a95
Othmane Nissoukin <othmane2000kaido@gmail.com>
2024-09-05 16:20:58 +0100

    added loading indicator

---
+++ app/account/loading.js
@@ -0,0 +1,11 @@
+import LoadingSpinner from "../_ui/LoadingSpinner";
+
+function Loading() {
+  return (
+    <div className="global-loading">
+      <LoadingSpinner />
+    </div>
+  );
+}
+
+export default Loading;
