14a2b7b04deaf923ac8721945bcf2ae853c3fde6
Othmane Nissoukin <othmane2000kaido@gmail.com>
2024-09-06 17:45:24 +0100

    added datepiker to rooms page and booking modal

--- app/_components/BookingForm/index.jsx
+++ app/_components/BookingForm/index.jsx
@@ -1,7 +1,13 @@
+"use client";
+import "react-datepicker/dist/react-datepicker.css";
+import DatePicker from "react-datepicker";
 import BookingButton from "../BookingButton";
 import styles from "./index.module.css";
+import { useState } from "react";
 
 function BookingForm({ children }) {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
   return (
     <form className={styles.bookingForm}>
       <h1 className={styles.formHeading}>BOOK A ROOM ONLINE</h1>
@@ -9,13 +15,32 @@ function BookingForm({ children }) {
         <label htmlFor="" className={styles.formLabel}>
           Arrival
         </label>
-        <input type="date" name="" id="" className={styles.input} />
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          selectsStart
+          startDate={startDate}
+          endDate={endDate}
+          className={styles.input}
+          dateFormat={"dd/MM/yyyy"}
+          excludeDateIntervals={[{ start: new Date("01/01/1970"), end: new Date() }]}
+        />
       </div>
       <div className={styles.formControl}>
         <label htmlFor="" className={styles.formLabel}>
           Departure
         </label>
-        <input type="date" name="" id="" className={styles.input} />
+        <DatePicker
+          selected={endDate}
+          onChange={(date) => setEndDate(date)}
+          selectsEnd
+          startDate={startDate}
+          endDate={endDate}
+          minDate={startDate}
+          className={styles.input}
+          dateFormat={"dd/MM/yyyy"}
+          excludeDateIntervals={[{ start: new Date("01/01/1970"), end: new Date() }]}
+        />
       </div>
 
       <div className={styles.actions}>
--- app/rooms/_components/FilterSection/index.jsx
+++ app/rooms/_components/FilterSection/index.jsx
@@ -2,8 +2,11 @@
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import styles from "./styles.module.css";
+import "react-datepicker/dist/react-datepicker.css";
 
 import Select from "react-select";
+import DatePicker from "react-datepicker";
+import { useState } from "react";
 
 const options = [
   { value: "default", label: "Default Sorting" },
@@ -14,6 +17,9 @@ const options = [
 ];
 
 function FilterSection() {
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
@@ -26,15 +32,47 @@ function FilterSection() {
 
   return (
     <form className={`${styles.filterSection}`}>
-      <Select
-        onChange={(e) => {
-          handleSearch(e);
-        }}
-        options={options}
-        isSearchable={false}
-        className={styles.select}
-        defaultValue={options.at(0)}
-      />
+      <div className={styles.formControl}>
+        <label>Filter By Date</label>
+        <div className={styles.datesContainer}>
+          <DatePicker
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            selectsStart
+            startDate={startDate}
+            endDate={endDate}
+            className={styles.input}
+            dateFormat={"dd/MM/yyyy"}
+            excludeDateIntervals={[{ start: new Date("01/01/1970"), end: new Date() }]}
+            placeholderText="Arrival Date"
+          />
+          <DatePicker
+            selected={endDate}
+            onChange={(date) => setEndDate(date)}
+            selectsEnd
+            startDate={startDate}
+            endDate={endDate}
+            minDate={startDate}
+            className={styles.input}
+            dateFormat={"dd/MM/yyyy"}
+            excludeDateIntervals={[{ start: new Date("01/01/1970"), end: new Date() }]}
+            placeholderText="Departure Date"
+          />
+        </div>
+      </div>
+
+      <div className={styles.formControl}>
+        <label htmlFor="">Sort Rooms</label>
+        <Select
+          onChange={(e) => {
+            handleSearch(e);
+          }}
+          options={options}
+          isSearchable={false}
+          className={styles.select}
+          defaultValue={options.at(0)}
+        />
+      </div>
     </form>
   );
 }
