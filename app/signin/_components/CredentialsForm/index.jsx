3577efd4167eb4e7da00dca0e81d4c4ea901b4a5
Othmane Nissoukin <othmane2000kaido@gmail.com>
2024-09-16 21:17:33 +0100

    removed login default values

--- app/signin/_components/CredentialsForm/index.jsx
+++ app/signin/_components/CredentialsForm/index.jsx
@@ -34,14 +34,14 @@ function CredentialsForm({ authAction }) {
         <label htmlFor="" className={styles.loginLabel}>
           Email Address
         </label>
-        <input type="email" name="email" defaultValue={"trigger2000p@gmail.com"} className={styles.loginInput} />
+        <input type="email" name="email" className={styles.loginInput} />
         {/* {state.email && <span className={styles.errorMessage}>{state.email}</span>} */}
       </div>
       <div className={styles.formControl}>
         <label htmlFor="" className={styles.loginLabel}>
           Password
         </label>
-        <input type="password" name="password" defaultValue={"azerty"} className={styles.loginInput} />
+        <input type="password" name="password" className={styles.loginInput} />
         {/* {state.password && <span className={styles.errorMessage}>{state.password}</span>} */}
       </div>
 
